07da2bce001be5bdeb9da9129deeb7f67437c9db
Ben Thomas <bendude56@gmail.com>
2014-12-20 23:52:47 -0700

    Implemented the ability to set ISR/IRQ handlers

--- src/kernel/core/idt.cpp
+++ src/kernel/core/idt.cpp
@@ -37,17 +37,19 @@ namespace idt
     const uint16 COMMAND_ICW4 = 0x01;
     
     const uint16 COMMAND_EOI = 0x20;
+    const uint16 COMMAND_READ_ISR = 0x0A;
     
     idt_ptr pointer;
     idt_entry entries[NUM_IDT_ENTRIES];
     
     interrupt_handler default_isr_handler;
-    interrupt_handler isr_handlers[32];
-    interrupt_handler irq_handlers[16];
+    interrupt_handler isr_handlers[NUM_ISRS];
+    interrupt_handler irq_handlers[NUM_IRQS];
+    interrupt_handler ext_handlers[NUM_EXT];
     
     extern "C" void idt_flush(idt_ptr* pointer);
     
-    void remap_pic(uint8 offset_master, uint8 offset_slave)
+    static void remap_pic(uint8 offset_master, uint8 offset_slave)
     {
         // Save the mask registers (they are cleared by initialization, and must be
         // restored later)
@@ -80,7 +82,7 @@ namespace idt
         outb(SLAVE_PIC_DATA, mask_slave);
     }
     
-    void mask_all_pic_irqs()
+    static void mask_all_pic_irqs()
     {
         // Set the IMR on the master and slave PICs to have all interrupts masked
         // (all bits set)
@@ -88,7 +90,7 @@ namespace idt
         outb(SLAVE_PIC_DATA, 0xFF);
     }
     
-    void set_pic_irq_masked(uint8 irq, bool masked)
+    static void set_pic_irq_masked(uint8 irq, bool masked)
     {
         // IRQs 0-7 are on the master PIC and IRQs 8-15 are on the slave PIC
         uint16 port = (irq > 7) ? SLAVE_PIC_DATA : MASTER_PIC_DATA;
@@ -118,10 +120,12 @@ namespace idt
         // be called using INT from userland))
         
         // Load the ISR handlers into the IDT.
-        for (int i = 0; i < 32; i++) set(i, (uint32) _isr_handlers[i], 0x08, 0x8E);
+        for (uint32 i = 0; i < NUM_ISRS; i++) set(i, (uint32) _isr_handlers[i], 0x08, 0x8E);
         
         // Load the IRQ handlers into the IDT
-        for (int i = 0; i < 16; i++) set(i + 32, (uint32) _irq_handlers[i], 0x08, 0x8E);
+        for (uint32 i = 0; i < NUM_IRQS; i++) set(i + 32, (uint32) _irq_handlers[i], 0x08, 0x8E);
+        
+        // TODO: Load extended interrupt handlers into the IDT
         
         // Flush the IDT to the CPU
         idt_flush(&pointer);
@@ -133,7 +137,7 @@ namespace idt
         // Remap the PIC to have IRQs 0-7 at IDT entries 0x20-0x27 and IRQs 8-15 at
         // IDT entries 0x28-0x2F. If we don't do this, then IRQs 0-7 conflict with
         // ISRs 8-15, and we won't be able to differentiate them!
-        remap_pic(0x20, 0x28);
+        remap_pic(IRQS_START, IRQS_START + 0x08);
     }
     
     void set(uint16 n, uint32 offset, uint16 selector, uint8 flags)
@@ -150,9 +154,114 @@ namespace idt
         entries[n].flags = flags;
     }
     
+    void register_isr_handler(uint8 n, interrupt_handler handler)
+    {
+        if (n >= NUM_ISRS)
+            crash("Attempt to set ISR handler for non-existent ISR!");
+        
+        isr_handlers[n] = handler;
+    }
+    
+    void register_default_isr_handler(interrupt_handler handler)
+    {
+        default_isr_handler = handler;
+    }
+    
+    void register_irq_handler(uint16 n, interrupt_handler handler)
+    {
+        if (n >= NUM_IRQS)
+            crash("Attempt to set IRQ handler for non-existent IRQ!");
+        
+        irq_handlers[n] = handler;
+    }
+    
+    void set_irq_enabled(uint8 n, bool enabled)
+    {
+        if (n >= NUM_IRQS)
+            crash("Attempt to enable/disable non-existent IRQ!");
+        
+        set_pic_irq_masked(n, !enabled);
+    }
+    
+    void register_extended_handler(uint8 n, interrupt_handler handler)
+    {
+        if (n >= NUM_EXT)
+            crash("Attempt to set extended interrupt handler beyond maximum interrupt!");
+        
+        ext_handlers[n] = handler;
+    }
+    
+    static uint16 read_pic_isr()
+    {
+        // Send the ISR read command to the master and slave PICs.
+        outb(MASTER_PIC_COMMAND, COMMAND_READ_ISR);
+        outb(SLAVE_PIC_COMMAND, COMMAND_READ_ISR);
+        
+        // Read the results back from the two PICs.
+        return (inb(SLAVE_PIC_COMMAND) << 8) | inb(MASTER_PIC_COMMAND);
+    }
+    
+    static bool irq_begin(regs32_t* reg)
+    {
+        // IRQs 7 and 15 may be spurious. Check for that before handling the IRQ.
+        if (reg->int_no - IRQS_START == 7 && (read_pic_isr() & (1 << 7)) == 0)
+        {
+            return false;
+        }
+        else if (reg->int_no - IRQS_START == 15 && (read_pic_isr() & (1 << 15)) == 0)
+        {
+            // The slave PIC knows that this is a spurious IRQ, but the master PIC
+            // does not. Thus, we must send an EOI to the master PIC.
+            outb(MASTER_PIC_COMMAND, COMMAND_EOI);
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    static void irq_end(regs32_t* reg)
+    {
+        // If the IRQ came from the slave PIC, let it know we're done with the
+        // interrupt.
+        if (reg->int_no - IRQS_START > 7) outb(SLAVE_PIC_COMMAND, COMMAND_EOI);
+        
+        // Always let the master PIC know we're done with the interrupt.
+        outb(MASTER_PIC_COMMAND, COMMAND_EOI);
+    }
+    
     extern "C" void _idt_handle(regs32_t* reg)
     {
-        // TODO: Handle interrupt
+        interrupt_handler handler = 0;
+        
+        // Find the proper interrupt handler and perform IRQ pre-handling code
+        if (reg->int_no < NUM_ISRS)
+        {
+            handler = isr_handlers[reg->int_no];
+            
+            if (handler == 0) handler = default_isr_handler;
+        }
+        else if (reg->int_no >= IRQS_START && reg->int_no < IRQS_START + NUM_IRQS)
+        {
+            handler = irq_handlers[reg->int_no];
+            
+            if (!irq_begin(reg)) return;
+        }
+        else if (reg->int_no >= EXT_INT_START && reg->int_no < EXT_INT_START + NUM_EXT)
+        {
+            handler = ext_handlers[reg->int_no];
+        }
+        
+        // Call the ISR handler or crash if an ISR was unhandled.
+        if (handler != 0) (*handler)(reg);
+        else if (reg->int_no < NUM_ISRS) crash("Unhandled ISR!");
+        
+        // If we were handling an IRQ, we need to perform IRQ post-handling
+        if (reg->int_no >= IRQS_START && reg->int_no < IRQS_START + NUM_IRQS)
+        {
+            irq_end(reg);
+        }
     }
     
     extern "C" bool _idt_is_ring0(regs32_t* reg)
--- src/kernel/include/core/idt.hpp
+++ src/kernel/include/core/idt.hpp
@@ -7,6 +7,14 @@ namespace idt
 {
     const uint32 NUM_IDT_ENTRIES = 256;
     
+    const uint32 NUM_ISRS = 32;
+    
+    const uint32 IRQS_START = 0x20;
+    const uint32 NUM_IRQS = 16;
+    
+    const uint32 EXT_INT_START = 0x80;
+    const uint32 NUM_EXT = 0; // TODO: Add extended interrupts
+    
     typedef void (*interrupt_handler)(regs32_t* reg);
     
     struct idt_entry
@@ -24,14 +32,16 @@ namespace idt
         uint32 base;
     } __attribute__((packed));
     
-    void init();
-    void set(uint16 n, uint32 offset, uint16 selector, uint8 flags);
+    extern void init();
+    extern void set(uint16 n, uint32 offset, uint16 selector, uint8 flags);
     
-    void register_isr_handler(uint8 n, interrupt_handler handler);
-    void register_default_isr_handler(interrupt_handler handler);
+    extern void register_isr_handler(uint8 n, interrupt_handler handler);
+    extern void register_default_isr_handler(interrupt_handler handler);
     
-    void register_irq_handler(uint16 n, interrupt_handler handler);
-    void set_irq_enabled(uint8 n, bool enabled);
+    extern void register_irq_handler(uint16 n, interrupt_handler handler);
+    extern void set_irq_enabled(uint8 n, bool enabled);
+    
+    extern void register_extended_handler(uint8 n, interrupt_handler handler);
 }
 
 #endif
