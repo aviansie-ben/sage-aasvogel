1bb5931168dd5817e399c2a7185d25bbd3825c97
bendude56 <bendude56@gmail.com>
2015-06-17 00:34:02 -0600

    Fixed the emulate.sh script for newer versions of QEMU

--- emulate.sh
+++ emulate.sh
@@ -1,1 +1,1 @@
-qemu-system-i386 -fda sa.img -m 64 -s -S -cpu Westmere,-de,-syscall,-lm,enforce -smp 2 -serial stdio
+qemu-system-i386 -fda sa.img -m 64 -s -S -cpu Westmere,-de,-syscall,-lm,-vme,enforce -smp 2 -serial stdio
