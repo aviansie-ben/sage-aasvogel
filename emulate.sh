911432c39d4a515c75951f3bd5654c1529c66320
bendude56 <bendude56@gmail.com>
2015-08-02 17:20:46 -0600

    Fixed up emulate.sh to explicitly load the floppy image as a raw image file

--- emulate.sh
+++ emulate.sh
@@ -1,1 +1,1 @@
-qemu-system-i386 -fda sa.img -m 64 -s -S -cpu Westmere,-de,-syscall,-lm,-vme,enforce -smp 2 -serial stdio
+qemu-system-i386 -drive file=sa.img,if=floppy,format=raw -m 64 -s -S -cpu Westmere,-de,-syscall,-lm,-vme,enforce -smp 2 -serial stdio
