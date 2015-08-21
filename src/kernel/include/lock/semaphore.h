7e27253ca599ad62e91c75b6378410e9e72ba802
bendude56 <bendude56@gmail.com>
2015-08-20 22:48:54 -0600

    Add a simple semaphore implementation

---
+++ src/kernel/include/lock/semaphore.h
@@ -0,0 +1,22 @@
+#ifndef LOCK_SEMAPHORE_H
+#define LOCK_SEMAPHORE_H
+
+#include <typedef.h>
+#include <lock/spinlock.h>
+#include <core/sched.h>
+
+typedef struct semaphore
+{
+    spinlock lock;
+    
+    int value;
+    sched_thread_queue wait_queue;
+} semaphore;
+
+extern void semaphore_init(semaphore* s, int value);
+
+extern void semaphore_wait(semaphore* s);
+extern bool semaphore_try_wait(semaphore* s);
+extern void semaphore_signal(semaphore* s);
+
+#endif
---
+++ src/kernel/lock/semaphore.c
@@ -0,0 +1,70 @@
+#include <lock/semaphore.h>
+
+void semaphore_init(semaphore* s, int value)
+{
+    spinlock_init(&s->lock);
+    
+    s->value = value;
+    sched_thread_queue_init(&s->wait_queue);
+}
+
+void semaphore_wait(semaphore* s)
+{
+    sched_thread* t = sched_thread_current();
+    uint32 eflags = eflags_save();
+    asm volatile ("cli");
+    
+    spinlock_acquire(&s->lock);
+    
+    if (s->value-- <= 0)
+    {
+        spinlock_acquire(&s->wait_queue.lock);
+        t->status = STS_BLOCKING;
+        sched_thread_enqueue(&s->wait_queue, t);
+        spinlock_release(&s->wait_queue.lock);
+        spinlock_release(&s->lock);
+        sched_yield();
+    }
+    else
+    {
+        spinlock_release(&s->lock);
+    }
+    
+    eflags_load(eflags);
+}
+
+bool semaphore_try_wait(semaphore* s)
+{
+    spinlock_acquire(&s->lock);
+    
+    if (s->value > 0)
+    {
+        s->value--;
+        spinlock_release(&s->lock);
+        return true;
+    }
+    else
+    {
+        spinlock_release(&s->lock);
+        return false;
+    }
+}
+
+void semaphore_signal(semaphore* s)
+{
+    sched_thread* t;
+    
+    spinlock_acquire(&s->lock);
+    
+    if (s->value++ < 0)
+    {
+        spinlock_acquire(&s->wait_queue.lock);
+        
+        t = sched_thread_dequeue(&s->wait_queue);
+        sched_thread_wake(t);
+        
+        spinlock_release(&s->wait_queue.lock);
+    }
+    
+    spinlock_release(&s->lock);
+}
