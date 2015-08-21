184b1ee065c29899d832c4c583f3294f6707562c
bendude56 <bendude56@gmail.com>
2015-08-21 14:44:16 -0600

    Flush the klog buffer before crashing. Display more crash information over the serial port.

--- src/kernel/core/crash.c
+++ src/kernel/core/crash.c
@@ -6,6 +6,7 @@
 #include <core/unwind.h>
 
 #include <core/ksym.h>
+#include <core/klog.h>
 
 static void lookup_name(unsigned int address, char* out)
 {
@@ -44,6 +45,7 @@ void do_crash(const char* msg, const char* file, const char* func, uint32 line)
 {
     asm volatile ("cli");
     
+    klog_flush();
     tty_switch_vc(&tty_virtual_consoles[0]);
     
     tty_virtual_consoles[0].fore_color = CONSOLE_COLOR_WHITE;
@@ -52,7 +54,7 @@ void do_crash(const char* msg, const char* file, const char* func, uint32 line)
     tty_virtual_consoles[0].base.cursor_hidden = true;
     
     tprintf(&tty_virtual_consoles[0].base, "Sage Aasvogel has crashed!\n  %s\n  Location: %s line %d (%s)\n\nStack Trace:\n", msg, file, line, func);
-    tprintf(&tty_serial_consoles[0].base, "Kernel has crashed!\nStack Trace:\n");
+    tprintf(&tty_serial_consoles[0].base, "Kernel has crashed: %s\nStack Trace:\n", msg);
     unchecked_unwind_here(1, CRASH_STACKTRACE_MAX_DEPTH, crash_print_stackframe);
     
     hang();
@@ -62,16 +64,16 @@ void do_crash_interrupt(const char* msg, const char* file, const char* func, uin
 {
     asm volatile ("cli");
     
+    klog_flush();
     tty_switch_vc(&tty_virtual_consoles[0]);
     
-    spinlock_acquire(&tty_virtual_consoles[0].base.lock);
     tty_virtual_consoles[0].fore_color = CONSOLE_COLOR_WHITE;
     tty_virtual_consoles[0].back_color = CONSOLE_COLOR_RED;
     tty_virtual_consoles[0].base.clear(&tty_virtual_consoles[0].base);
     tty_virtual_consoles[0].base.cursor_hidden = true;
-    spinlock_release(&tty_virtual_consoles[0].base.lock);
     
     tprintf(&tty_virtual_consoles[0].base, "Sage Aasvogel has crashed!\n  %s\n  Location: %s line %d\n  Function: %s\n\nStack Trace:\n", msg, file, line, func);
+    tprintf(&tty_serial_consoles[0].base, "Kernel has crashed!\nStack Trace:\n");
     unchecked_unwind(r->eip, (void*) r->ebp, CRASH_STACKTRACE_MAX_DEPTH, crash_print_stackframe);
     
     hang();
@@ -84,39 +86,45 @@ void do_crash_pagefault(regs32_t* r)
     asm volatile ("cli");
     asm volatile ("movl %%cr2, %0" : "=r" (fault_address));
     
+    klog_flush();
     tty_switch_vc(&tty_virtual_consoles[0]);
     
-    spinlock_acquire(&tty_virtual_consoles[0].base.lock);
     tty_virtual_consoles[0].fore_color = CONSOLE_COLOR_WHITE;
     tty_virtual_consoles[0].back_color = CONSOLE_COLOR_RED;
     tty_virtual_consoles[0].base.clear(&tty_virtual_consoles[0].base);
     tty_virtual_consoles[0].base.cursor_hidden = true;
-    spinlock_release(&tty_virtual_consoles[0].base.lock);
     
     tprintf(&tty_virtual_consoles[0].base, "Sage Aasvogel has crashed!\n  ");
+    tprintf(&tty_serial_consoles[0].base, "Kernel has crashed: ");
     
     if ((r->err_code & 0x8) != 0)
     {
         tprintf(&tty_virtual_consoles[0].base, "The page at 0x%08x has reserved bits set!", fault_address);
+        tprintf(&tty_serial_consoles[0].base, "reserved bits set in page");
     }
     else if ((r->err_code & 0x1) == 0)
     {
         tprintf(&tty_virtual_consoles[0].base, "Attempt to %s non-present memory at 0x%08x!", ((r->err_code & 0x10) != 0) ? "execute" : (((r->err_code & 0x2) != 0) ? "write" : "read"), fault_address);
+        tprintf(&tty_serial_consoles[0].base, "attempt to %s non-present memory", ((r->err_code & 0x10) != 0) ? "execute" : (((r->err_code & 0x2) != 0) ? "write" : "read"));
     }
     else if ((r->err_code & 0x10) != 0)
     {
         tprintf(&tty_virtual_consoles[0].base, "Attempt to execute non-executable memory at 0x%08x!", fault_address);
+        tprintf(&tty_serial_consoles[0].base, "executing non-executable memory");
     }
     else if ((r->err_code & 0x2) != 0)
     {
         tprintf(&tty_virtual_consoles[0].base, "Attempt to write to read-only memory at 0x%08x!", fault_address);
+        tprintf(&tty_serial_consoles[0].base, "writing read-only memory");
     }
     else
     {
         tprintf(&tty_virtual_consoles[0].base, "Attempt to read protected memory at 0x%08x!", fault_address);
+        tprintf(&tty_serial_consoles[0].base, "reading protected memory");
     }
     
     tprintf(&tty_virtual_consoles[0].base, "\n\nStack Trace:\n");
+    tprintf(&tty_serial_consoles[0].base, "\nFaulting address: 0x%08x\nStack Trace:\n", fault_address);
     unchecked_unwind(r->eip, (void*) r->ebp, CRASH_STACKTRACE_MAX_DEPTH, crash_print_stackframe);
     
     hang();
@@ -136,6 +144,7 @@ void do_crash_unhandled_isr(regs32_t* r)
     spinlock_release(&tty_virtual_consoles[0].base.lock);
     
     tprintf(&tty_virtual_consoles[0].base, "Sage Aasvogel has crashed!\n  Unexpected ISR: 0x%x\n\nStack Trace:\n", r->int_no);
+    tprintf(&tty_serial_consoles[0].base, "Kernel has crashed: unhandled isr 0x%x\nStack Trace:\n", r->int_no);
     unchecked_unwind(r->eip, (void*) r->ebp, CRASH_STACKTRACE_MAX_DEPTH, crash_print_stackframe);
     
     hang();
