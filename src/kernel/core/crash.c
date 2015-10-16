f09a48a74ba9432117d6447a7ce9345dc8db6e94
bendude56 <bendude56@gmail.com>
2015-10-15 19:46:07 -0600

    Attempt to get a symbol for the faulting address when a pagefault occurs

--- src/kernel/core/crash.c
+++ src/kernel/core/crash.c
@@ -8,20 +8,22 @@
 #include <core/ksym.h>
 #include <core/klog.h>
 
-static void lookup_name(unsigned int address, char* out)
+static bool lookup_name(unsigned int address, char* out, uint32 flags)
 {
     char tmp[9];
     
     const kernel_symbol* symbol;
     uint32 symbol_offset;
     
-    if ((symbol = ksym_address_lookup(address, &symbol_offset, KSYM_ALOOKUP_RET)) != NULL)
+    if ((symbol = ksym_address_lookup(address, &symbol_offset, flags)) != NULL)
     {
         itoa((int) symbol_offset, tmp, 16);
         
         strcpy(out, symbol->name);
         strcat(out, "+0x");
         strcat(out, tmp);
+        
+        return true;
     }
     else
     {
@@ -29,6 +31,8 @@ static void lookup_name(unsigned int address, char* out)
         
         strcpy(out, "0x");
         strcat(out, tmp);
+        
+        return false;
     }
 }
 
@@ -36,7 +40,7 @@ static void crash_print_stackframe(unsigned int eip, void* esp)
 {
     char name[50];
     
-    lookup_name(eip, name);
+    lookup_name(eip, name, KSYM_ALOOKUP_RET);
     tprintf(&tty_virtual_consoles[0].base, "  %s\n", name);
     tprintf(&tty_serial_consoles[0].base, "  %s\n", name);
 }
@@ -82,6 +86,8 @@ void do_crash_interrupt(const char* msg, const char* file, const char* func, uin
 void do_crash_pagefault(regs32_t* r)
 {
     uint32 fault_address;
+    char address_name[50];
+    bool address_named;
     
     asm volatile ("cli");
     asm volatile ("movl %%cr2, %0" : "=r" (fault_address));
@@ -94,37 +100,50 @@ void do_crash_pagefault(regs32_t* r)
     tty_virtual_consoles[0].base.clear(&tty_virtual_consoles[0].base);
     tty_virtual_consoles[0].base.cursor_hidden = true;
     
+    address_named = lookup_name(fault_address, address_name, 0);
+    
     tprintf(&tty_virtual_consoles[0].base, "Sage Aasvogel has crashed!\n  ");
     tprintf(&tty_serial_consoles[0].base, "Kernel has crashed: ");
     
     if ((r->err_code & 0x8) != 0)
     {
-        tprintf(&tty_virtual_consoles[0].base, "The page at 0x%08x has reserved bits set!", fault_address);
+        tprintf(&tty_virtual_consoles[0].base, "Reserved bits set in page at ", fault_address);
         tprintf(&tty_serial_consoles[0].base, "reserved bits set in page");
     }
     else if ((r->err_code & 0x1) == 0)
     {
-        tprintf(&tty_virtual_consoles[0].base, "Attempt to %s non-present memory at 0x%08x!", ((r->err_code & 0x10) != 0) ? "execute" : (((r->err_code & 0x2) != 0) ? "write" : "read"), fault_address);
+        tprintf(&tty_virtual_consoles[0].base, "Attempt to %s non-present memory at ", ((r->err_code & 0x10) != 0) ? "execute" : (((r->err_code & 0x2) != 0) ? "write" : "read"));
         tprintf(&tty_serial_consoles[0].base, "attempt to %s non-present memory", ((r->err_code & 0x10) != 0) ? "execute" : (((r->err_code & 0x2) != 0) ? "write" : "read"));
     }
     else if ((r->err_code & 0x10) != 0)
     {
-        tprintf(&tty_virtual_consoles[0].base, "Attempt to execute non-executable memory at 0x%08x!", fault_address);
+        tprintf(&tty_virtual_consoles[0].base, "Attempt to execute non-executable memory at ");
         tprintf(&tty_serial_consoles[0].base, "executing non-executable memory");
     }
     else if ((r->err_code & 0x2) != 0)
     {
-        tprintf(&tty_virtual_consoles[0].base, "Attempt to write to read-only memory at 0x%08x!", fault_address);
+        tprintf(&tty_virtual_consoles[0].base, "Attempt to write to read-only memory at ");
         tprintf(&tty_serial_consoles[0].base, "writing read-only memory");
     }
     else
     {
-        tprintf(&tty_virtual_consoles[0].base, "Attempt to read protected memory at 0x%08x!", fault_address);
+        tprintf(&tty_virtual_consoles[0].base, "Attempt to read protected memory at ");
         tprintf(&tty_serial_consoles[0].base, "reading protected memory");
     }
     
+    if (address_named)
+    {
+        tprintf(&tty_virtual_consoles[0].base, "%s [0x%08x]", address_name, fault_address);
+        tprintf(&tty_serial_consoles[0].base, "\nFaulting address: %s [0x%08x]", address_name, fault_address);
+    }
+    else
+    {
+        tprintf(&tty_virtual_consoles[0].base, "0x%08x", fault_address);
+        tprintf(&tty_serial_consoles[0].base, "\nFaulting address: 0x%08x", fault_address);
+    }
+    
     tprintf(&tty_virtual_consoles[0].base, "\n\nStack Trace:\n");
-    tprintf(&tty_serial_consoles[0].base, "\nFaulting address: 0x%08x\nStack Trace:\n", fault_address);
+    tprintf(&tty_serial_consoles[0].base, "\nStack Trace:\n", fault_address, address_name);
     unchecked_unwind(r->eip, (void*) r->ebp, CRASH_STACKTRACE_MAX_DEPTH, crash_print_stackframe);
     
     hang();
