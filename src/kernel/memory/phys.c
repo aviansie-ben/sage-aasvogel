b07f659bf974ac755f288b22a663b51c5d3a0330
Benjamin Thomas (Aviansie Ben) <ben@benthomas.ca>
2016-07-18 05:26:28 +0000

    Allow frames under 1MiB to be marked as free
    Previously, all frames before the end of the kernel would always be
    marked as "KERNEL RESERVED". However, for some uses (e.g. initializing
    SMP), it's important to be able to allocate frames <1MiB.
    
    In order to accomodate this, the way that kernel reserved frames are
    found has been changed. This will now allow frames in low memory to be
    added to the free frame stack.

--- src/kernel/memory/phys.c
+++ src/kernel/memory/phys.c
@@ -10,6 +10,18 @@
 #define EMERG_STACK_SIZE 128
 #define FRAMES_PER_STACK_FRAME ((FRAME_SIZE / sizeof(addr_p)) - 1)
 
+extern const void _ld_kernel_begin;
+extern const void _ld_kernel_end;
+extern const void _ld_kmalloc_early_begin;
+
+typedef struct
+{
+    addr_p start_address;
+    addr_p end_address;
+    
+    const char* name;
+} kernel_resv_region;
+
 typedef struct
 {
     addr_p next_stack_frame;
@@ -156,22 +168,107 @@ static addr_p _alloc_frame(frame_alloc_flags flags)
     }
 }
 
-static void _push_unallocated(const boot_param* param)
+static void _push_region_frames(addr_p region_start, addr_p region_end, bool* high_warn)
+{
+    assert((region_start & FRAME_MASK) == 0);
+    assert((region_end & FRAME_MASK) == 0);
+    
+    if (region_end > (1ull << 32) && !high_stack_enabled)
+    {
+        if (!*high_warn)
+        {
+            klog(KLOG_LEVEL_WARN, "Memory beyond 4GiB detected, but unusable since PAE is disabled\n");
+            *high_warn = true;
+        }
+        
+        region_end = (1ull << 32);
+    }
+    
+    for (addr_p addr = region_start; addr != region_end; addr += FRAME_SIZE)
+        _push_free_frame(addr);
+}
+
+static void _push_region_non_reserved_frames(addr_p region_start, addr_p region_end, bool* high_warn, size_t num_resv_regions, const kernel_resv_region* resv_regions)
+{
+    assert((region_start & FRAME_MASK) == 0);
+    assert((region_end & FRAME_MASK) == 0);
+    
+    for (size_t i = 0; i < num_resv_regions; i++)
+    {
+        const kernel_resv_region* rr = &resv_regions[i];
+        
+        if (rr->end_address > region_start && rr->end_address < region_end)
+        {
+            if (rr->start_address > region_start)
+                _push_region_non_reserved_frames(region_start, rr->start_address, high_warn, num_resv_regions, resv_regions);
+            
+#ifdef MMAP_DEBUG
+            klog(
+                KLOG_LEVEL_DEBUG,
+                "mmap: 0x%016lx -> 0x%016lx - %s\n",
+                rr->start_address > region_start ? rr->start_address : region_start,
+                rr->end_address,
+                rr->name
+            );
+#endif
+            
+            _push_region_non_reserved_frames(rr->end_address, region_end, high_warn, num_resv_regions, resv_regions);
+            return;
+        }
+        else if (rr->start_address > region_start && rr->start_address < region_end)
+        {
+            _push_region_non_reserved_frames(region_start, rr->end_address, high_warn, num_resv_regions, resv_regions);
+            
+#ifdef MMAP_DEBUG
+            klog(
+                KLOG_LEVEL_DEBUG,
+                "mmap: 0x%016lx -> 0x%016lx - %s\n",
+                rr->start_address,
+                rr->end_address < region_end ? rr->end_address : region_end,
+                rr->name
+            );
+#endif
+            
+            if (rr->end_address < region_end)
+                _push_region_non_reserved_frames(rr->end_address, region_end, high_warn, num_resv_regions, resv_regions);
+            
+            return;
+        }
+        else if (rr->start_address <= region_start && rr->end_address >= region_end)
+        {
+#ifdef MMAP_DEBUG
+            klog(
+                KLOG_LEVEL_DEBUG,
+                "mmap: 0x%016lx -> 0x%016lx - %s\n",
+                region_start,
+                region_end,
+                rr->name
+            );
+#endif
+            return;
+        }
+    }
+    
+#ifdef MMAP_DEBUG
+    klog(
+        KLOG_LEVEL_DEBUG,
+        "mmap: 0x%016lx -> 0x%016lx - FREE\n",
+        region_start,
+        region_end
+    );
+#endif
+    
+    _push_region_frames(region_start, region_end, high_warn);
+}
+
+static void _push_unallocated(const boot_param* param, size_t num_resv_regions, const kernel_resv_region* resv_regions)
 {
     bool high_warn = false;
-    addr_p alloc_end = (addr_p)(kmem_early_next_alloc - 0xC0000000);
     
     const boot_param_mmap_region* region;
     
     addr_p region_begin;
     addr_p region_end;
-    addr_p addr;
-    
-    if ((alloc_end & 0xfffull) != 0)
-    {
-        alloc_end |= 0xfffull;
-        alloc_end += 1;
-    }
     
     for (size_t i = 0; i < param->num_mmap_regions; i++)
     {
@@ -194,46 +291,7 @@ static void _push_unallocated(const boot_param* param)
         
         if (region->type == 1)
         {
-            if (region_end > alloc_end)
-            {
-                if (region_begin < alloc_end)
-                {
-#ifdef MMAP_DEBUG
-                    klog(KLOG_LEVEL_DEBUG, "mmap: 0x%016lx -> 0x%016lx - KERNEL RESERVED\n", region_begin, alloc_end);
-                    klog(KLOG_LEVEL_DEBUG, "mmap: 0x%016lx -> 0x%016lx - FREE\n", alloc_end, region_end);
-#endif
-                    region_begin = alloc_end;
-                }
-#ifdef MMAP_DEBUG
-                else
-                {
-                    klog(KLOG_LEVEL_DEBUG, "mmap: 0x%016lx -> 0x%016lx - FREE\n", region_begin, region_end);
-                }
-#endif
-                
-                for (addr = region_begin; addr != region_end; addr += 0x1000)
-                {
-                    if (addr >= (1ull << 32) && !high_stack_enabled)
-                    {
-                        if (!high_warn)
-                        {
-                            klog(KLOG_LEVEL_WARN, "Memory beyond 4GiB detected, but unusable since PAE is disabled\n");
-                            high_warn = true;
-                        }
-                        
-                        kmem_total_frames -= (uint32)((region_end - addr) / FRAME_SIZE);
-                        break;
-                    }
-                    
-                    _push_free_frame(addr);
-                }
-            }
-#ifdef MMAP_DEBUG
-            else
-            {
-                klog(KLOG_LEVEL_DEBUG, "mmap: 0x%016lx -> 0x%016lx - KERNEL RESERVED\n", region_begin, region_end);
-            }
-#endif
+            _push_region_non_reserved_frames(region_begin, region_end, &high_warn, num_resv_regions, resv_regions);
         }
 #ifdef MMAP_DEBUG
         else
@@ -253,7 +311,13 @@ void kmem_phys_init(const boot_param* param)
     high_stack.next_stack_frame = FRAME_NULL;
     high_stack_top = 0;
     
-    _push_unallocated(param);
+    kernel_resv_region resv_regions[] = {
+        { 0x0, 0x1000, "NULL FRAME" },
+        { (addr_p)(uint32) &_ld_kernel_begin, (addr_p)(uint32) &_ld_kernel_end, "KERNEL BIN" },
+        { (addr_p)(uint32) &_ld_kmalloc_early_begin - 0xC0000000, (addr_p) kmem_early_next_alloc - 0xC0000000, "KMALLOC_EARLY" }
+    };
+    
+    _push_unallocated(param, sizeof(resv_regions) / sizeof(*resv_regions), resv_regions);
     
     klog(KLOG_LEVEL_INFO, "Found %dKiB of memory, with %dKiB free.\n", kmem_total_frames * 4, kmem_free_frames * 4);
     init_done = true;
