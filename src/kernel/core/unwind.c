dddb3ccdbe7e9071d52e0c4bf57a7c94598f35ce
Benjamin Thomas (Aviansie Ben) <ben@benthomas.ca>
2016-11-27 03:37:55 +0000

    Prevent unchecked_unwind from raising page faults
    Previously, a corrupt stack could cause the unchecked_unwind function
    to raise page faults. This would effectively obscure the cause of the
    problem, since the page fault error's details would be displayed instead
    of the details of the actual crash. By making unchecked_unwind handle
    page faults, the attempt to generate a stack trace will stop when a
    corrupt stack frame is encountered.

--- src/kernel/core/unwind.c
+++ src/kernel/core/unwind.c
@@ -1,30 +1,38 @@
 #include <core/unwind.h>
+#include <string.h>
 
-void unchecked_unwind(uint32 eip, void* ebp, uint32 max_frames, frame_handler handler)
+void unchecked_unwind(uint32 eip, void** ebp, uint32 max_frames, frame_handler handler)
 {
     while (ebp != 0 && max_frames != 0)
     {
-        (*handler)(eip, ebp);
+        (*handler)(eip, (void*)ebp);
         
-        eip = *(((unsigned int*) ebp) + 1);
-        ebp = *((void**) ebp);
+        if (!memcpy_safe(&eip, ebp + 1, sizeof(eip)) || !memcpy_safe(&ebp, ebp, sizeof(ebp)))
+            return;
+        
         max_frames--;
     }
 }
 
 void unchecked_unwind_here(uint32 skip_frames, uint32 max_frames, frame_handler handler)
 {
-    void* ebp;
+    uint32 eip;
+    void** ebp;
     
     asm volatile ("mov %%ebp, %0" : "=r" (ebp));
     
     while (skip_frames != 0)
     {
-        ebp = *((void**) ebp);
+        if (!memcpy_safe(&ebp, ebp, sizeof(ebp)))
+            return;
+        
         skip_frames--;
     }
     
-    unchecked_unwind(*(((unsigned int*) ebp) + 1), *((void**) ebp), max_frames, handler);
+    if (!memcpy_safe(&eip, ebp + 1, sizeof(eip)) || !memcpy_safe(&ebp, ebp, sizeof(ebp)))
+        return;
+    
+    unchecked_unwind(eip, ebp, max_frames, handler);
 }
 
 uint32 get_caller_address(void)
--- src/kernel/include/core/unwind.h
+++ src/kernel/include/core/unwind.h
@@ -5,7 +5,7 @@
 
 typedef void (*frame_handler)(unsigned int eip, void* ebp);
 
-void unchecked_unwind(uint32 eip, void* ebp, uint32 max_frames, frame_handler handler);
+void unchecked_unwind(uint32 eip, void** ebp, uint32 max_frames, frame_handler handler);
 void unchecked_unwind_here(uint32 skip_frames, uint32 max_frames, frame_handler handler);
 
 uint32 get_caller_address(void);
